4f2688f1af5a0b5b0e1bff7b986c9e185da7da8c
SAMIUL2022 <sami35bd@gmail.com>
2024-05-12 22:38:09 +0600

    .

---
+++ day_2/A_Maximum_GCD.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+#define op()                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+
+int main ()
+
+{
+op();
+  int t;
+  cin>>t;
+  while (t--)
+  {
+   int n;
+   cin>>n;
+   cout<<n/2<<endl;
+  }
+  
+
+
+    return 0;
+}
